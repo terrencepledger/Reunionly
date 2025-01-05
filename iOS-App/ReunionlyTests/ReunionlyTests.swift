32bb68d6129f3b1ceba280bddca1d1c8ff0e973e
terrencepledger <terrencepledger@gmail.com>
2025-01-04 21:26:40 -0600

    more cleanup

---
+++ iOS-App/ReunionlyTests/ReunionlyTests.swift
@@ -0,0 +1,16 @@
+//
+//  ReunionlyTests.swift
+//  ReunionlyTests
+//
+//  Created by Terrence Pledger on 1/4/25.
+//
+
+import Testing
+
+struct ReunionlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
